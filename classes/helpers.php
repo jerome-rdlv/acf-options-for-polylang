fc16e9d3521429786bf7365134b452281163d66f
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2026-01-13 17:25:45 +0100

    Fix when no languages are configured

--- classes/helpers.php
+++ classes/helpers.php
@@ -95,7 +95,11 @@ class Helpers {
 	 * @return bool
 	 */
 	public static function already_localized( $post_id ) {
-		preg_match( '/' . self::locales_regex_fragment() . '$/', $post_id, $language );
+		$locales_regex_fragment = self::locales_regex_fragment();
+		if ( ! $locales_regex_fragment ) {
+			return false;
+		}
+		preg_match( '/' . $locales_regex_fragment . '$/', $post_id, $language );
 
 		return ! empty( $language );
 	}
@@ -104,11 +108,16 @@ class Helpers {
 	 * @return string A regex fragment for all polylang configured locales
 	 */
 	public static function locales_regex_fragment(): string {
+		$locales = pll_languages_list( [ 'hide_empty' => false, 'fields' => 'locale' ] );
+		if ( ! $locales ) {
+			return '';
+		}
+
 		return sprintf(
 			'(%s)',
 			implode( '|', array_map( function ( $lang ) {
 				return preg_quote( $lang, '/' );
-			}, pll_languages_list( [ 'hide_empty' => false, 'fields' => 'locale' ] ) ) )
+			}, $locales ) )
 		);
 	}
 }
